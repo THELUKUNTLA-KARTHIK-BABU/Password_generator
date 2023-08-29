4c525e1466bba7791ce8c5aec8f794a11c516978
karthik <114253896+THELUKUNTLA-KARTHIK-BABU@users.noreply.github.com>
2023-08-30 00:25:05 +0530

    Add files via upload

---
+++ passwords.py
@@ -0,0 +1,34 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:/Users/karth/Desktop/python/projects.mini/Password_generator.py ==
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+0123456789
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+enter the password length
+5
+your password is:
+."L]8
+6
+6
+
+== RESTART: C:/Users/karth/Desktop/python/projects.mini/Password_generator.py ==
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+0123456789
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+enter the password length
+8
+your password is:
+\uMC0Jxm
+
+== RESTART: C:/Users/karth/Desktop/python/projects.mini/Password_generator.py ==
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+0123456789
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+enter the password length
+15
+your password is:
+=Onwh:C1U[Jt@c'
